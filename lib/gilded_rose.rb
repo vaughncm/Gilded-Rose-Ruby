4388a53c0de458d9f1bbe797d30c5761f6e9d70e
Chris Vaughn <vaughncm@gmail.com>
2024-03-16 14:36:05 -0400

    Quick pass at adding brevity to control flow

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,46 +9,35 @@ class GildedRose
 
   def tick
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
+      if @quality > 0 && @name != "Sulfuras, Hand of Ragnaros"
+        @quality = @quality - 1
       end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
+    elsif @quality < 50
+      @quality = @quality + 1
+      if @name == "Backstage passes to a TAFKAL80ETC concert"
+        if @days_remaining < 11 && @quality < 50
+          @quality = @quality + 1
+        end
+        if @days_remaining < 6 && @quality < 50
+          @quality = @quality + 1
         end
       end
     end
     if @name != "Sulfuras, Hand of Ragnaros"
       @days_remaining = @days_remaining - 1
     end
+
     if @days_remaining < 0
       if @name != "Aged Brie"
         if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
+          if @quality > 0 && @name != "Sulfuras, Hand of Ragnaros"
+            @quality = @quality - 1
           end
         else
           @quality = @quality - @quality
         end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
+      elsif @quality < 50
+        @quality = @quality + 1
       end
     end
   end
