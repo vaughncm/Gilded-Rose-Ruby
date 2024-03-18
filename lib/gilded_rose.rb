984a534850f3b93dca37188810f28ecae37b1bb4
Chris Vaughn <vaughncm@gmail.com>
2024-03-18 15:15:19 -0400

    adding some explanations with methods for what item adjustments do

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,35 +10,54 @@ class GildedRose
   def tick
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0 && @name != "Sulfuras, Hand of Ragnaros"
-        @quality = @quality - 1
+        decrement_quality
       end
     elsif @quality < 50
-      @quality = @quality + 1
+      increment_quality
       if @name == "Backstage passes to a TAFKAL80ETC concert"
         if @days_remaining < 11 && @quality < 50
-          @quality = @quality + 1
+          increment_quality
         end
         if @days_remaining < 6 && @quality < 50
-          @quality = @quality + 1
+          increment_quality
         end
       end
     end
     if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
+      reduce_days_left
     end
 
     if @days_remaining < 0
       if @name != "Aged Brie"
         if @name != "Backstage passes to a TAFKAL80ETC concert"
           if @quality > 0 && @name != "Sulfuras, Hand of Ragnaros"
-            @quality = @quality - 1
+            decrement_quality
           end
         else
-          @quality = @quality - @quality
+          expire_item
         end
       elsif @quality < 50
-        @quality = @quality + 1
+        increment_quality
       end
     end
   end
+
+  private
+
+  def increment_quality
+    @quality = @quality + 1
+  end
+
+  def decrement_quality
+    @quality = @quality - 1
+  end
+
+  def expire_item
+    @quality = @quality - @quality
+  end
+
+  def reduce_days_left
+    @days_remaining = @days_remaining - 1
+  end
+
 end
